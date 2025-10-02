5e23122d3b7096eacdf3d8d00aa079b86e0aabec
Thisanda <thisandaprasanjana2002@gmail.com>
2025-10-02 12:34:19 +0530

    init tailwindcss🤞

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import ProdcutCard from './components/productCard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <ProdcutCard name="Dog" price="20000" image="https://picsum.photos/id/237/200/300"/>
-    <ProdcutCard name="Cat" price="15000" image="https://picsum.photos/id/238/200/300"/>
-    <ProdcutCard name="Rabbit" price="10000" image="https://picsum.photos/id/239/200/300"/>
-    </>
+    <div className="w-65 h-[587.3px] border bg-pink-400">
+
+    </div>
   )
 }
 
---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import UserData from "./userData";
+
+export default function Header(){
+
+
+    return(
+        <header>
+        <h1>MY SHOP</h1>
+        <UserData/>
+        </header>
+        
+    )
+}
