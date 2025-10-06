6df70a3c9f622923e2ff8c8fa09b1bb5b89fc921
Thisanda <thisandaprasanjana2002@gmail.com>
2025-10-06 20:12:58 +0530

    tailwindcss learn😊

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,21 @@ import './App.css'
 function App() {
 
   return (
-    <div className="w-65 h-[587.3px] border bg-pink-400">
+    <div className="w-[700px] h-[700px] border bg-pink-400 relative" >
+      <div className='w-[500px] h-[500px] bg-yellow-500 flex flex-col justify-center items-center' >
+        <div className='w-[100px] h-[100px] bg-blue-500'>
+
+        </div>
+        <div className='w-[100px] h-[100px] bg-red-500 fixed left-[550px] top-[550px]'>
+
+        </div>
+        <div className='w-[100px] h-[100px] bg-green-500'>
+
+        </div>
+        <div className='w-[100px] h-[100px] bg-gray-500 absolute left-[300px] top-[300px]'>
+
+        </div>
+      </div> 
 
     </div>
   )
